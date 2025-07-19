db99f7551a1432038cb84ce0e4cee6667e282bb0
Alexey Danilchik <butuzz74@gmail.com>
2025-07-19 21:34:11 +0300

    fixed error 3

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -2,18 +2,19 @@ import { ProductsType } from "../../types/types";
 import ServiceCardForMainPage from "@/components/services/ServiceCardForServicePage";
 
 async function ServicesMainPage() {
-    const data = await fetch(
-        `${process.env.NEXT_PUBLIC_BASE_URL}/api/services`,
-        { cache: "no-store" }
-    );
+    // const data = await fetch(
+    //     `${process.env.NEXT_PUBLIC_BASE_URL}/api/services`,
+    //     { cache: "no-store" }
+    // );
 
-    const services: ProductsType = await data.json();
+    // const services: ProductsType = await data.json();
+
     return (
         <main className="flex min-h-screen flex-col items-center justify-around px-4 py-8 bg-gradient-to-b from-white to-blue-50">
             <h1 className="text-3xl font-bold text-blue-700 text-center mb-10">
                 Выбрать массаж
             </h1>
-            {services.products.length !== 0 &&
+            {/* {services.products.length !== 0 &&
                 services.products.map((elem) => (
                     <ServiceCardForMainPage
                         key={elem._id}
@@ -23,7 +24,7 @@ async function ServicesMainPage() {
                         duration={elem.duration}
                         linkForTransition={`/services/${elem._id}`}
                     />
-                ))}
+                ))} */}
         </main>
     );
 }
