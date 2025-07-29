6995d2dd0bcb25e94e3fbd28e1c354746bec7683
Alexey Danilchik <butuzz74@gmail.com>
2025-07-29 16:00:09 +0300

    add telegramId 2

--- models/Booking.ts
+++ models/Booking.ts
@@ -3,6 +3,7 @@ import { Schema, model, models } from "mongoose";
 const bookingSchema = new Schema(
     {
         massageId: { type: Schema.Types.ObjectId, ref: "Massage" },
+        telegramId: Number,
         massage: String,
         date: String,
         time: String,
